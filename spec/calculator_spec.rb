ed6b8a7b96b2d5a4ec4488a20ae2cbb219658b6e
Abdulrahman <boodynaser@rocketmail.com>
2021-04-07 14:57:51 +0200

    add subtract and divide tests

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,10 +7,22 @@ describe Calculator do
         expect(calculator.add(5, 2,7)).to eql(14)
       end
     end
-    describe "#multiply" do
-    it "return the product of more than two numbers" do
-        calculator = Calculator.new
-        expect(calculator.multiply(2,5)).to eql(10)
+    describe "#subtract" do
+    it "returns the subtraction of more than two numbers" do
+      calculator = Calculator.new
+      expect(calculator.subtract(5, 2,7)).to eql(-4)
     end
+  end
+    describe "#multiply" do
+        it "return the product of more than two numbers" do
+            calculator = Calculator.new
+            expect(calculator.multiply(2,5)).to eql(10)
+    end
+    end
+    describe "#divide" do
+        it "return the division of two numbers" do
+            calculator = Calculator.new
+            expect(calculator.divide(200,5)).to eql(40)
+        end
     end
 end
